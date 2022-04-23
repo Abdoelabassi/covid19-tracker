03d491cc6ba7616a52b169b050c8a9058ffedff3
Abdoelabassi <abderrezaq@outlook.com>
2022-04-23 14:28:51 +0000

    fetched&display cases and recoveries

--- src/App.js
+++ src/App.js
@@ -20,6 +20,22 @@ function App() {
 
   const [countries, setContries]  = useState([]);
   const [country, setCountry] = useState('Worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+
+
+//for worlwide cases
+
+useEffect(()=>{
+
+  fetch("https://disease.sh/v3/covid-19/all")
+  .then(res=>res.json())
+  .then(data=>{
+
+    setCountryInfo(data);
+  });
+},[])
+
+
 
   const fetchCovid = async ()=>{
 
@@ -52,8 +68,23 @@ function App() {
 
     const countryCode = event.target.value;
 
-    setCountry(countryCode)
+    
+
+    const url = countryCode === "Worldwide" ? "https://disease.sh/v3/covid-19/countriesall"
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+    await fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+      setCountry(countryCode);
+
+      setCountryInfo(data);
+
+
+    })
   }
+
+  console.log("Country info >>>> :", countryInfo)
 
   return (
     <div className="app"> {/*BEM convention of classeNames*/}
@@ -79,9 +110,9 @@ function App() {
 
           <div className="app__stats">
 
-            <InfoBox title="CoronaVirus Cases" cases={145} total={4000}/>
-            <InfoBox title="Recovered cases" cases={13} total={300} />
-            <InfoBox title="Deaths" cases={563} total={500}  />
+            <InfoBox title="CoronaVirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+            <InfoBox title="Recovered cases" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+            <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}  />
 
           </div>
 
